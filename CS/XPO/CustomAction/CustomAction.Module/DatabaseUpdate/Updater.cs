329db98e18db767f053fdbe016fecac0bb2d5ba7
andrey <andrey.kozhevnikov@devexpress.com>
2023-06-07 15:21:00 +0400

    xpo

---
+++ CS/XPO/CustomAction/CustomAction.Module/DatabaseUpdate/Updater.cs
@@ -0,0 +1,34 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.Data.Filtering;
+using DevExpress.Persistent.Base;
+using DevExpress.ExpressApp.Updating;
+using DevExpress.Xpo;
+using DevExpress.ExpressApp.Xpo;
+using DevExpress.Persistent.BaseImpl;
+using CustomActionControl.Module;
+
+namespace CustomAction.Module.DatabaseUpdate;
+
+// For more typical usage scenarios, be sure to check out https://docs.devexpress.com/eXpressAppFramework/DevExpress.ExpressApp.Updating.ModuleUpdater
+public class Updater : ModuleUpdater {
+    public Updater(IObjectSpace objectSpace, Version currentDBVersion) : base(objectSpace, currentDBVersion) { }
+    public override void UpdateDatabaseAfterUpdateSchema() {
+        base.UpdateDatabaseAfterUpdateSchema();
+        CreateOrder("0001", true, DateTime.Now);
+        CreateOrder("0002", true, DateTime.Now.AddDays(-1));
+        CreateOrder("0003", false, DateTime.Now.AddDays(-2));
+        CreateOrder("0004", false, DateTime.Now);
+        CreateOrder("0005", true, DateTime.Now.AddDays(-3));
+        ObjectSpace.CommitChanges();
+    }
+    private void CreateOrder(string orderNo, bool isActive, DateTime date) {
+        Order order = ObjectSpace.FindObject<Order>(new BinaryOperator("OrderNo", orderNo));
+        if (order == null) {
+            order = ObjectSpace.CreateObject<Order>();
+            order.OrderNo = orderNo;
+            order.Active = isActive;
+            order.Date = date;
+        }
+    }
+}
+
---
+++ CS/XPO/CustomAction/CustomAction.Win/ActionControls/CheckableSimpleActionBinding.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DevExpress.ExpressApp.Templates.ActionControls.Binding;
+
+namespace CustomActionControl.Module.Win.ActionControls {
+    public class CheckableSimpleActionBinding : ActionBinding {
+        private void Action_CheckedStateChanged(object sender, EventArgs e) {
+            ActionControl.Checked = Action.Checked;
+        }
+        private void ActionControl_Execute(object sender, EventArgs e) {
+            Action.Checked = !Action.Checked;
+        }
+        public CheckableSimpleActionBinding(CheckableSimpleAction action, BarCheckItemCheckableSimpleActionControl actionControl)
+            : base(action, actionControl) {
+            ActionControl.Execute += new EventHandler<EventArgs>(ActionControl_Execute);
+            Action.CheckedStateChanged += new EventHandler(Action_CheckedStateChanged);
+            ActionControl.Checked = Action.Checked;
+        }
+        public override void Dispose() {
+            ActionControl.Execute -= new EventHandler<EventArgs>(ActionControl_Execute);
+            Action.CheckedStateChanged -= new EventHandler(Action_CheckedStateChanged);
+            base.Dispose();
+        }
+        public new CheckableSimpleAction Action {
+            get { return (CheckableSimpleAction)base.Action; }
+        }
+        public new BarCheckItemCheckableSimpleActionControl ActionControl {
+            get { return (BarCheckItemCheckableSimpleActionControl)base.ActionControl; }
+        }
+    }
+}
